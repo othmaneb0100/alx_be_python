83458aaa45afbd074b47f5d7f2249199d4ff7c3e
boussaid <othmaneboussaid0100@gmail.com>
2024-06-22 17:50:57 +0100

    libghiti

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+number1=10
+number2 =5
+
+
+sum=number1 + number2
+subs=number1 - number2
+prod=number1 *number2
+
+print("Addition of", number1, "and", number2, "is", sum)
+print("Subtraction of", number1, "and", number2, "is", subs)
+print("Multiplication of", number1, "and", number2, "is", prod)
