da1c3d861e0d6337b6b037f8de29f1be41fb72dc
boussaid <othmaneboussaid0100@gmail.com>
2024-06-22 18:58:12 +0100

    bonu v8

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,9 @@
+income = int(input("Enter your monthly income:"))
+expenses = int(input("Enter your total monthly expenses:"))
+
+Monthly_Savings = income - expenses 
+100
+Projected_Savings = Monthly_Savings * 12 + (Monthly_Savings * 12 * 0.05)
+
+print("Your monthly savings are ", "$",Monthly_Savings , ".")
+print("Projected savings after one year, with interest, is:", "$" ,Projected_Savings ,  ".")
