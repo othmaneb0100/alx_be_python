2966b9f031093f8c090a61a9886dad568e65f4d0
othmaneb0100 <othmaneboussaid0100@gmail.com>
2024-07-07 00:40:29 +0000

    fn

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -39,3 +39,15 @@ def main():
 
 if __name__ == "__main__":
     main()
+ 
+
+
+
+# shopping_list_manager.py
+
+def display_menu():
+    print("\nShopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
